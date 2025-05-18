113d82ace37e94559538315a075da96cbbc8788c
Vitalii Beiar <37002507+nightven@users.noreply.github.com>
2025-05-18 12:45:06 -0230

    test for Ostap

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -3,3 +3,6 @@ import { Request, Response } from 'express';
 export const getExample = (req: Request, res: Response) => {
   res.json({ message: 'Example route works!' });
 };
+export const getTest = (req: Request, res: Response) => {
+  res.json({ message: 'Example route works!' });
+};
