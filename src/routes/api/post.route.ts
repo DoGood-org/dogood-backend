69a22a95ed3284cd7d33e530326a0d3e94c89850
Yuliia Voziian <vozianulia@gmail.com>
2025-06-30 22:58:43 +0300

    fix: remove duplicate imports and handlers

--- src/routes/api/post.route.ts
+++ src/routes/api/post.route.ts
@@ -4,7 +4,6 @@ import {
   getFilteredPosts,
 } from '@/controllers/posts.controller';
 import { validateBody, validateIdParam } from '@/middlewares';
-import { validateBody, validateIdParam } from '@/middlewares';
 import { schemas } from '@/schemas/post.schemas';
 import { Router } from 'express';
 
@@ -13,7 +12,6 @@ export const postRoute = Router();
 postRoute
   .route('/posts')
   .get(getFilteredPosts)
-  .post(validateBody(schemas.createPostSchema), createPost);
   .post(validateBody(schemas.createPostSchema), createPost);
 
 postRoute.route('/posts/:id').get(validateIdParam, getPostById);
