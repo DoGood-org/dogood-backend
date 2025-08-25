7f7652911678c0f24d046de0d3fcd01916e3c0d8
sofiialives <127687828+sofiialives@users.noreply.github.com>
2025-08-25 20:20:28 +0200

    reviews route and schema

--- src/routes/api/review.route.ts
+++ src/routes/api/review.route.ts
@@ -1,30 +1,27 @@
 import { Router } from 'express';
-import {controllers} from "@/controllers/reviews.controller";
-import {authenticateUser, validateBody, validateIdParam} from "@/middlewares";
-import { schemas } from "@/schemas/review.schema";
+import { controllers } from '@/controllers/reviews.controller';
+import { authenticateUser, validateBody, validateIdParam } from '@/middlewares';
+import { schemas } from '@/schemas/review.schema';
+import { validateQuery } from '@/middlewares/chat.middleware';
 
 export const reviewsRoute = Router();
 
 reviewsRoute.post(
-    '/',
-    validateBody(schemas.createReviewSchema),
-    authenticateUser, controllers.createReview
+  '/',
+  validateBody(schemas.createReviewSchema),
+  authenticateUser,
+  controllers.createReview
 );
+
+reviewsRoute.get('/:id', controllers.getReviewById);
+
+reviewsRoute.get('/by-user/:id', validateIdParam, controllers.getUserReviews);
+
+reviewsRoute.delete('/:id', authenticateUser, controllers.deleteReview);
 
 reviewsRoute.get(
-    '/:id',
-    controllers.getReviewById
+  '/all',
+  authenticateUser,
+  validateQuery(schemas.getReviewsSchema),
+  controllers.getReviews
 );
-
-reviewsRoute.get(
-    '/by-user/:id',
-    validateIdParam,
-    controllers.getUserReviews
-);
-
-reviewsRoute.delete(
-    '/:id',
-    authenticateUser,
-    controllers.deleteReview
-);
-
--- src/schemas/review.schema.ts
+++ src/schemas/review.schema.ts
@@ -1,23 +1,30 @@
 import { z } from 'zod';
 
 const createReviewSchema = z.object({
-    authorId: z
-        .number({ invalid_type_error: 'authorId must be a number' })
-        .int()
-        .positive({ message: 'authorId must be a positive integer' }),
-    targetId: z
-        .number({ invalid_type_error: 'targetId must be a number' })
-        .int()
-        .positive({ message: 'targetId must be a positive integer' }),
-    rating: z
-        .number({ invalid_type_error: 'Rating must be a number' }).min(1).max(5)
-        .int()
-        .positive({ message: 'rating must be a positive integer' }),
-    comment: z
-        .string({invalid_type_error: 'Comment must be a string',})
-        .optional()
-        .nullable()
-
+  authorId: z
+    .number({ invalid_type_error: 'authorId must be a number' })
+    .int()
+    .positive({ message: 'authorId must be a positive integer' }),
+  targetId: z
+    .number({ invalid_type_error: 'targetId must be a number' })
+    .int()
+    .positive({ message: 'targetId must be a positive integer' }),
+  rating: z
+    .number({ invalid_type_error: 'Rating must be a number' })
+    .min(1)
+    .max(5)
+    .int()
+    .positive({ message: 'rating must be a positive integer' }),
+  comment: z
+    .string({ invalid_type_error: 'Comment must be a string' })
+    .optional()
+    .nullable(),
 });
 
-export const schemas = { createReviewSchema };
+const getReviewsSchema = z.object({
+  type: z.enum(['user', 'organisation', 'platform']).optional(),
+  target_id: z.string().optional(),
+  status: z.enum(['approved', 'pending', 'rejected']).optional(),
+});
+
+export const schemas = { createReviewSchema, getReviewsSchema };
