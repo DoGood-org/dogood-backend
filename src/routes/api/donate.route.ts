fbe32637bc20352d8b9a367c2e00b45feb652cbb
sofiialives <127687828+sofiialives@users.noreply.github.com>
2025-09-03 00:10:17 +0200

    schemas for routes

--- src/routes/api/donate.route.ts
+++ src/routes/api/donate.route.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { controllers } from '@/controllers/donate.controller';
+import { validateBody, validateIdParam } from '@/middlewares';
+import { schemas } from '../../schemas/donate.schema';
 
 export const donateRoute = express.Router();
 
-donateRoute.get('/:id', controllers.getDonation);
+donateRoute.get('/:id', validateIdParam, controllers.getDonation);
 
-donateRoute.post('/create-checkout-session', controllers.createCheckoutSession);
+donateRoute.post(
+  '/create-checkout-session',
+  validateBody(schemas.createDonateSchema),
+  controllers.createCheckoutSession
+);
 
 donateRoute.post(
   '/webhook',
