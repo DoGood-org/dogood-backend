734c5be372c3de32343ed61bad221823a058c386
Vitalii Beiar <37002507+nightven@users.noreply.github.com>
2025-09-05 20:59:01 -0230

    update file names

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,10 @@
+export * from './auth.middleware';
+export * from './authorizeTaskAction.middleware';
+export * from './chat.middleware';
+export * from './rateLimit.middleware';
 export * from './validateBody.middleware';
 export * from './validateId.middleware';
-export * from './auth.middleware';
+export * from './validateSocketData.middleware';
+
+
+
