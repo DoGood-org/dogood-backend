48642a39aa0deff712e6865bd0f8158eae822860
Vitalii Beiar <37002507+nightven@users.noreply.github.com>
2025-11-17 19:21:07 -0330

    added update route

--- src/routes/api/review.route.ts
+++ src/routes/api/review.route.ts
@@ -50,13 +50,6 @@ reviewsRoute.post(
   controllers.createTaskUserReviewController
 );
 
-reviewsRoute.patch(
-  '/:id',
-  authenticateUser,
-  validateBody(schemas.updateReviewSchema), 
-  controllers.updateReview
-);
-
 reviewsRoute.get('/:id', controllers.getReviewById);
 
 reviewsRoute.get('/by-user/:id', validateIdParam, controllers.getUserReviews);
